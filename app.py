d2c419c07b0cc3896dd51f857f5927eca6b4f5e7
Athira Suresh <athirasuresh338@gmail.com>
2026-06-18 23:04:51 +0530

    Added image upload functionality

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 import streamlit as st
+from PIL import Image
 
-st.title("Interactive Image Compression Explorer")
+st.title("Image Compression Studio")
 
-st.write("Welcome to the Image Compression Explorer")
+uploaded_file = st.file_uploader(
+    "Upload an image",
+    type=["jpg", "jpeg", "png"]
+)
+
+if uploaded_file is not None:
+
+    image = Image.open(uploaded_file)
+
+    st.subheader("Original Image")
+
+    st.image(
+        image,
+        use_container_width=True
+    )
